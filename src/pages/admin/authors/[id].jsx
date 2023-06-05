0f8978b8da6f407a3150998b671b1a09864d47b7
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-05 20:51:50 +0700

    feat: add admin/authors and [id]

---
+++ src/pages/admin/authors/[id].jsx
@@ -0,0 +1,109 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function BookDetailsAdminPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [author, setAuthor] = useState({
+    authorname: "",
+    authorid: "",
+    yearborn: "",
+    yeardied: "",
+  });
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  useEffect(() => {
+    if (id) {
+      axios
+        .get(`${apiUrl}/authors/${id}`)
+        .then((res) => {
+          setAuthor(res.data[0]);
+        })
+        .catch((err) => {
+          console.log(err);
+          // alert("Error. Make sure the API is running.");
+        });
+    }
+  }, [id]);
+
+  return (
+    <main className="flex flex-col items-center min-h-screen py-10">
+      <div className="w-1/4">
+        <form>
+          <div className="">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Author Name
+            </label>
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+              placeholder="Book Name"
+              value={author.authorname}
+              onChange={(e) =>
+                setAuthor({ ...author, bookname: e.target.value })
+              }
+            />
+          </div>
+          <div className="mt-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Year Born
+            </label>
+            <input
+              type="number"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+              placeholder="Year Born"
+              value={author.yearborn}
+              onChange={(e) =>
+                setAuthor({ ...author, yearborn: e.target.value })
+              }
+            />
+          </div>
+          <div className="mt-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Year Died
+            </label>
+            <input
+              type="number"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+              placeholder="Year Died"
+              value={author.yeardied == null? "" : author.yeardied}
+              onChange={(e) =>
+                setAuthor({ ...author, yeardied: e.target.value })
+              }
+            />
+          </div>
+        </form>
+
+        <button
+          className="w-full bg-green-400 py-2 mt-10 cursor-pointer hover:bg-green-500 rounded-md"
+          onClick={() => {
+            axios
+              .post(`${apiUrl}/books/update/${id}`, author)
+              .then((res) => {
+                alert("Book updated successfully");
+                router.push("/admin/books");
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }}
+        >
+          Update
+        </button>
+        <button
+          className="w-full bg-red-400 py-2 mt-5 cursor-pointer hover:bg-red-500 rounded-md"
+          onClick={() => alert("dari mana duitnya")}
+        >
+          Delete
+        </button>
+        <button
+          className="w-full bg-blue-400 py-2 mt-5 cursor-pointer hover:bg-blue-500 rounded-md"
+          onClick={() => router.push("/admin/authors")}
+        >
+          &larr; Back
+        </button>
+      </div>
+    </main>
+  );
+}
---
+++ src/pages/admin/authors/index.jsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { BsSearch } from "react-icons/bs";
+
+export default function AdminBookPage() {
+  const [authors, setAuthors] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [refresh, setRefresh] = useState(false); 
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  useEffect(() => {
+    axios
+      .get(`${apiUrl}/authors`)
+      .then((res) => {
+        setAuthors(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        // alert("Error. Make sure the API is running.");
+      });
+  }, [refresh]);
+
+  return (
+    <main className="min-h-screen py-20">
+      <h1 className="text-center text-[20px] font-bold mb-5">Admin - Authors</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (searchKey === "") {
+            setRefresh(!refresh);
+          } else {
+            const result = authors.filter((author) =>
+              author.authorname
+                .toLowerCase()
+                .includes(searchKey.trim().toLowerCase())
+            );
+            setAuthors(result);
+          }
+        }}
+        className="flex w-fit mx-auto mb-5"
+      >
+        <input
+          type="text"
+          className="outline outline-1 rounded-l-md px-2 py-1"
+          value={searchKey}
+          onChange={(e) => setSearchKey(e.target.value)}
+        />
+        <button type="submit" className="outline outline-1 px-2 rounded-r-md">
+          <BsSearch />
+        </button>
+      </form>
+      <div className="grid grid-cols-4 gap-5 mx-[20%]">
+        {authors.map((author) => (
+          <AuthorCard key={author.authorid} author={author} />
+        ))}
+      </div>
+    </main>
+  );
+}
+
+function AuthorCard({ author }) {
+  return (
+    <Link href={`/admin/authors/${author.authorid}`}>
+      <div className="aspect-square grid place-items-center outline outline-black bg-white cursor-pointer rounded-md transition hover:drop-shadow-[0_0_5px_rgba(0,0,0,.9)]">
+        <h1 className="text-center">{author.authorname}</h1>
+      </div>
+    </Link>
+  );
+}
