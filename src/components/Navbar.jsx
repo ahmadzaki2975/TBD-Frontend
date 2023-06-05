a0c07ac2d967972977610f799024a185e5935ef2
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-05 07:14:01 +0700

    feat: create Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function Navbar() {
+  const router = useRouter();
+
+  return (
+    <nav className="flex justify-between absolute top-0 w-full bg-blue-500 py-4 px-5 text-white">
+      <Link href="/">
+        <div className="font-bold">BookstoreDB</div>
+      </Link>
+      <div className="flex gap-5">
+        <Link href="/">Book</Link>
+        <Link href="/">Authors</Link>
+        <Link href="/">Publishers</Link>
+      </div>
+    </nav>
+  );
+}
