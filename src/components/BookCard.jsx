438a1128c4898879058bffd3b44dfc60e01a9862
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-05 19:30:36 +0700

    feat: add hover effects

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,6 +1,6 @@
 export default function BookCard({ book }) {
   return (
-    <a href={`/book/${book.bookid}`} className="w-full block cursor-pointer">
+    <a href={`/book/${book.bookid}`} className="w-full block cursor-pointer hover:drop-shadow-[0_0_10px_rgb(139,92,246)]">
       <div className="relative w-full aspect-[3/4] h-[250px] bg-gradient-to-br from-violet-500 to-blue-500">
         <div className="absolute bottom-0 bg-gradient-to-b from-transparent from-[30%] to-white/90 w-full pt-10 px-2 pb-1">
           <h1 className="text-/center font-bold">{book.bookname}</h1>
