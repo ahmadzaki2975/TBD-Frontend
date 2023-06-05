811dc2b5c2458df65eeecd737d79ec9eba152da9
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-05 07:13:55 +0700

    feat: create Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <div className="py-3 grid place-items-center bg-blue-500 text-white font-semibold">
+            &copy; Ahmad Zaki Akmal 2023
+        </div>
+    )
+}
