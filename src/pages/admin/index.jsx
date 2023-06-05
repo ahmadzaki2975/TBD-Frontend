0183f8626a9a8dbe57608e7ffa2045e632df9ed5
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-05 19:30:50 +0700

    feat: add admin page

---
+++ src/pages/admin/index.jsx
@@ -0,0 +1,25 @@
+export default function Admin() {
+  return(
+    <main className="min-h-screen py-24">
+      <h1 className="text-center text-[20px] font-bold mb-10">Admin</h1>
+      <div className="grid grid-cols-4 gap-5 mx-[20%]">
+        <MenuItem text="Books" />
+        <MenuItem text="Authors" />
+        <MenuItem text="Publishers" />
+        <MenuItem text="Staffs" />
+        <MenuItem text="Customers" />
+        <MenuItem text="Orders" />
+        <MenuItem text="Stores" />
+        <MenuItem text="Genres" />
+      </div>
+    </main>
+  )
+}
+
+function MenuItem({text}) {
+  return(
+    <div className="aspect-square grid place-items-center outline outline-black bg-white cursor-pointer rounded-md transition hover:drop-shadow-[0_0_5px_rgba(0,0,0,.9)]">
+      {text}
+    </div>
+  )
+}
