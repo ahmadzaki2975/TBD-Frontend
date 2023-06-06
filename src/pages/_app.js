0efb2d4b78205ae97acc89239141afb3e5740a2f
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-06 08:34:14 +0700

    feat: initialize toast

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <ToastContainer 
+        position="top-center"
+        theme="dark"
+      />
       <Navbar />
       <Component {...pageProps} />
       <Footer />
