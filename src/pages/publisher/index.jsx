efd2cef9e140e2995959ef5ccb3defe00dffa151
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-06 09:10:15 +0700

    fix: missing key

--- src/pages/publisher/index.jsx
+++ src/pages/publisher/index.jsx
@@ -11,7 +11,7 @@ export default function Publisher() {
       .then((res) => {
         setPublishers(res.data);
       })
-      .then((err) => console.log(err));
+      .catch((err) => console.log(err));
   }, []);
 
   return (
@@ -19,7 +19,7 @@ export default function Publisher() {
       <h1 className="text-center text-[20px] font-bold mb-10">Publishers</h1>
       <div className="grid grid-cols-4 gap-5">
         {publishers.map((publisher) => {
-          return <PublisherCard key={publisher.id} publisher={publisher} />;
+          return <PublisherCard key={publisher.publishername} publisher={publisher} />;
         })}
       </div>
     </main>
