f216c2db87bb09c58f1c416bd57afd74765e699c
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-06 19:23:52 +0700

    feat: add drop shadow on new book button

--- src/pages/admin/books/index.jsx
+++ src/pages/admin/books/index.jsx
@@ -67,7 +67,7 @@ export default function AdminBookPage() {
         ))}
       </div>
       <Link href="/admin/books/new">
-        <button className="text-[40px] fixed right-0 bottom-0 m-10 text-blue-500 hover:text-blue-600">
+        <button className="text-[40px] drop-shadow-[0_0_5px_#FFFFFF] fixed right-0 bottom-0 m-10 text-blue-500 hover:text-blue-600">
           <BsFillPlusCircleFill />
         </button>
       </Link>
