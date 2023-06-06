e7dac4f383731f854ac1289e789f389007fd9bca
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-06 11:31:00 +0700

    feat: add author details page

--- src/components/AuthorCard.jsx
+++ src/components/AuthorCard.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import { BsPersonFill } from "react-icons/bs";
 
 export default function AuthorCard({ author }) {
   return (
-    <div className="w-fit">
+    <Link href={`/author/${author.authorid}`}>
+      <div className="w-fit">
       <div className="h-[250px] relative grid place-items-center aspect-[3/4] bg-gradient-to-tr from-green-500 to-yellow-300">
         <BsPersonFill className="absolute text-white text-[50px]" />
         <div className="absolute bottom-0 bg-gradient-to-b from-transparent from-[30%] to-white/90 w-full pt-10 px-2 pb-1">
@@ -10,5 +12,6 @@ export default function AuthorCard({ author }) {
         </div>
       </div>
     </div>
+    </Link>
   );
 }
---
+++ src/pages/author/[id].jsx
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function AuthorDetailPage() {
+  const [author, setAuthor] = useState({});
+  const [bookwritten, setBookWritten] = useState([]);
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    if (id) {
+      axios
+        .get(`${apiUrl}/authors/${id}`)
+        .then((response) => {
+          setAuthor(response.data[0]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      axios
+        .get(`${apiUrl}/authors/books/${id}`)
+        .then((response) => {
+          setBookWritten(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, []);
+  return (
+    <main className="flex flex-col items-center min-h-screen py-10">
+      <div className="w-[150px] aspect-[3/4] bg-gradient-to-br from-violet-500 to-blue-500 text-white grid place-items-center">
+        Ini gambar
+      </div>
+      <div className="w-1/4">
+        <h1 className="font-bold text-center text-[20px] mb-3">
+          {author.authorname}
+        </h1>
+        <h2 className="text-center text-[16px] font-semibold">Name</h2>
+        <h2 className="text-center text-[16px]">{author.authorname}</h2>
+        <hr className="my-1" />
+        <h2 className="text-center text-[16px] font-semibold">Year of Birth</h2>
+        <h2 className="text-center text-[16px]">{author.yearborn}</h2>
+        <hr className="my-1" />
+        <h2 className="text-center text-[16px] font-semibold">Year of Death</h2>
+        <h2 className="text-center text-[16px]">
+          {author.yeardeath ? author.yeardeath : "Alive"}
+        </h2>
+        <hr className="my-1" />
+        <h2 className="text-center text-[16px] font-semibold">Books Written ({bookwritten.length})</h2>
+        <h2 className="text-center text-[16px]">{bookwritten.map(book => {
+          return book.bookname
+        })}</h2>
+
+        <button
+          className="w-full bg-green-400 py-2 mt-10 cursor-pointer hover:bg-green-500 rounded-md"
+          onClick={() => alert("dari mana duitnya")}
+        >
+          Buy for {author.price}$ each
+        </button>
+      </div>
+    </main>
+  );
+}
