41f657105cc7e9be2d985537b7d3044b21809ac3
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-07 05:25:56 +0700

    feat: changed admin menu

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -18,14 +18,14 @@ export default function Admin() {
       <h1 className="text-center text-[20px] font-bold mb-10">Admin</h1>
       {isAuthenticated ? (
         <div className="grid grid-cols-4 gap-5 mx-[20%]">
-          <MenuItem text="Books" />
-          <MenuItem text="Authors" />
-          <MenuItem text="Publishers" />
-          <MenuItem text="Staffs" />
-          <MenuItem text="Customers" />
-          <MenuItem text="Orders" />
-          <MenuItem text="Stores" />
-          <MenuItem text="SQL" />
+          <MenuItem text="Books" link="books" />
+          <MenuItem text="Authors" link="authors" />
+          <MenuItem text="Publishers" link="publishers" />
+          <MenuItem text="Staffs" link="staffs" />
+          <MenuItem text="Customers" link="customers" />
+          <MenuItem text="Orders" link="orders" />
+          <MenuItem text="Stores" link="stores" />
+          <MenuItem text="SQL Injection Simulator" link="sql"/>
         </div>
       ) : (
         <form onSubmit={(e) => {
@@ -58,11 +58,11 @@ export default function Admin() {
   );
 }
 
-function MenuItem({ text }) {
+function MenuItem({ text, link }) {
   return (
-    <Link href={`/admin/${text.toLowerCase()}`}>
-      <div className="aspect-square grid place-items-center outline outline-black bg-white cursor-pointer rounded-md transition hover:drop-shadow-[0_0_5px_rgba(0,0,0,.9)]">
-        {text}
+    <Link href={`/admin/${link}`}>
+      <div className="px-5 aspect-square grid place-items-center outline outline-black bg-white cursor-pointer rounded-md transition hover:drop-shadow-[0_0_5px_rgba(0,0,0,.9)]">
+        <h1 className="text-center">{text}</h1>
       </div>
     </Link>
   );
