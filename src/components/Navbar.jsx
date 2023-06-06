89d32969b75dcda55c4eef79f752c9e03a763134
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-06 10:36:06 +0700

    feat: add current route indicator

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const router = useRouter();
+  const [pathname, setPathname] = useState("");
+
+  useEffect(() => {
+    setPathname(router.pathname);
+  }, [router.pathname]);
 
   return (
     <nav className="flex justify-between //absolute top-0 w-full bg-blue-500 py-4 px-5 text-white">
@@ -10,10 +16,10 @@ export default function Navbar() {
         <div className="font-bold">BookstoreDB</div>
       </Link>
       <div className="flex gap-5">
-        <Link href="/">Books</Link>
-        <Link href="/author">Authors</Link>
-        <Link href="/publisher">Publishers</Link>
-        <Link href="/admin">Admin</Link>
+        <Link className={pathname == "/" ? "underline underline-offset-4" : ""} href="/">Books</Link>
+        <Link className={pathname == "/author" ? "underline underline-offset-4" : ""} href="/author">Authors</Link>
+        <Link className={pathname == "/publisher" ? "underline underline-offset-4" : ""} href="/publisher">Publishers</Link>
+        <Link className={pathname.includes("/admin") ? "underline underline-offset-4" : ""} href="/admin">Admin</Link>
       </div>
     </nav>
   );
