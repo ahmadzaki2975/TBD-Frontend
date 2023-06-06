c9a89a4ad51493c87a662712f4d7710bdc7284e4
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-06 09:50:51 +0700

    feat: add backdrop blur

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,7 +5,7 @@ export default function Loading() {
   const { loading } = useContext(LoadingContext);
   if (loading)
     return (
-      <div className="bg-black/70 w-screen h-screen fixed top-0 z-[99] grid place-items-center">
+      <div className="bg-black/70 w-screen backdrop-blur-[8px] h-screen fixed top-0 z-[99] grid place-items-center">
         <h1 className="text-[30px] text-white animate-pulse">ksabar...</h1>
       </div>
     );
