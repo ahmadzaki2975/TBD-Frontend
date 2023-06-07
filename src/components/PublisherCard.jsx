2a82f6396f73cefe55a4efe5bda0c32432fc4925
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-07 08:14:51 +0700

    feat: added link

--- src/components/PublisherCard.jsx
+++ src/components/PublisherCard.jsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function PublisherCard({ publisher }) {
   return (
-    <div className="flex justify-center items-center flex-col">
-      <div className="bg-gradient-to-br from-red-400 to-yellow-500 aspect-square w-[120px] rounded-full grid place-items-center">
-        <h1 className="text-center">{publisher.publishername}</h1>
+    <Link href={`/publisher/${publisher.publishername}`}>
+      <div className="flex justify-center items-center flex-col text-white">
+        <div className="bg-gradient-to-br from-red-400 to-yellow-500 aspect-square w-[120px] rounded-full grid place-items-center">
+          <h1 className="text-center">{publisher.publishername}</h1>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
