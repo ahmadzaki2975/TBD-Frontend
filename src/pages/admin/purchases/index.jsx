18a839eb8f34f2cc7fb5c73eaf8aac885cb40e18
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-07 07:26:41 +0700

    feat: create purchases page

---
+++ src/pages/admin/purchases/index.jsx
@@ -0,0 +1,101 @@
+import { LoadingContext } from "@/contexts/LoadingContext";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useContext, useEffect, useState } from "react";
+import { BsFillPlusCircleFill, BsSearch } from "react-icons/bs";
+import { toast } from "react-toastify";
+
+export default function AdminBookPage() {
+  const [purchases, setPurchases] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [refresh, setRefresh] = useState(false);
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const router = useRouter();
+  const { loading, setLoading } = useContext(LoadingContext);
+  useEffect(() => {
+    setLoading(true);
+    if (localStorage.getItem("isAuthenticated")) {
+      axios
+        .get(`${apiUrl}/purchases`)
+        .then((res) => {
+          console.log(res.data)
+          setPurchases(res.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          // alert("Error. Make sure the API is running.");
+        });
+    } else {
+      router.push("/admin");
+      toast.error("Admin resources, access denied.");
+    }
+  }, [refresh]);
+
+  return (
+    <main className="min-h-screen py-20 relative">
+      <h1 className="text-center text-[20px] font-bold mb-5">
+        Admin - Stores
+      </h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (searchKey === "") {
+            setRefresh(!refresh);
+          } else {
+            const result = purchases.filter((publisher) =>
+              publisher.publishername
+                .toLowerCase()
+                .includes(searchKey.trim().toLowerCase())
+            );
+            setPurchases(result);
+          }
+        }}
+        className="flex w-fit mx-auto mb-5"
+      >
+        <input
+          type="text"
+          className="outline outline-1 rounded-l-md px-2 py-1"
+          value={searchKey}
+          onChange={(e) => setSearchKey(e.target.value)}
+        />
+        <button type="submit" className="outline outline-1 px-2 rounded-r-md">
+          <BsSearch />
+        </button>
+      </form>
+      <div className="flex flex-col gap-5 w-1/2 mx-auto">
+        {purchases.map((purchase) => (
+          <BookCard key={purchase.orderid} customer={purchase} />
+        ))}
+      </div>
+      <Link href="/admin/books/new">
+        <button className="text-[40px] drop-shadow-[0_0_5px_#FFFFFF] fixed right-0 bottom-0 m-10 text-blue-500 hover:text-blue-600">
+          <BsFillPlusCircleFill />
+        </button>
+      </Link>
+    </main>
+  );
+}
+
+function BookCard({ customer: purchase }) {
+  return (
+    <div className="p-2 outline outline-black bg-white cursor-pointer rounded-md transition hover:drop-shadow-[0_0_5px_rgba(0,0,0,.9)]">
+      <h1 className="font-bold">Order ID: {purchase.orderid}</h1>
+      <h2 className="font-semibold">Ordered By</h2>
+      <p>{purchase.customername}</p>
+      <hr className="my-1" />
+      <h2 className="font-semibold">Book Ordered</h2>
+      <p>{purchase.bookname}</p>
+      <hr className="my-1" />
+      <h2 className="font-semibold">Quantity</h2>
+      <p>{purchase.quantity}</p>
+      <hr className="my-1" />
+      <h2 className="font-semibold">Total Price</h2>
+      <p>{purchase.totalprice}</p>
+      <hr className="my-1" />
+      <h2 className="font-semibold">Order Date</h2>
+      <p>{purchase.orderdate}</p>
+    </div>
+  );
+}
