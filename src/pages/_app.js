b6d0548b428ce8fd7ccc794b4a8d21a98eb874ca
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-08 05:17:24 +0700

    feat: add title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,21 +3,24 @@ import Loading from "@/components/Loading";
 import Navbar from "@/components/Navbar";
 import { LoadingContext, LoadingProvider } from "@/contexts/LoadingContext";
 import "@/styles/globals.css";
+import Head from "next/head";
 import { useContext } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <LoadingProvider>
-      <Loading />
-      <ToastContainer 
-        position="bottom-right"
-        theme="dark"
-      />
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </LoadingProvider>
+    <>
+      <Head>
+        <title>Good Reading Bookstore</title>
+      </Head>
+      <LoadingProvider>
+        <Loading />
+        <ToastContainer position="bottom-right" theme="dark" />
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </LoadingProvider>
+    </>
   );
 }
